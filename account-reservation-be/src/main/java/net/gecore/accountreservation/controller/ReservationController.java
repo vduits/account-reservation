38e245f7ce2bc1d585e26632f15abe2ab223fc9e
Victor <vdcommits@outlook.com>
2019-10-08 05:04:43 +0200

    add endpoint mapping and allow crossorigin with angular dev for now

--- account-reservation-be/src/main/java/net/gecore/accountreservation/controller/ReservationController.java
+++ account-reservation-be/src/main/java/net/gecore/accountreservation/controller/ReservationController.java
@@ -4,10 +4,14 @@ import java.time.LocalDateTime;
 import net.gecore.accountreservation.domain.Reservation;
 import net.gecore.accountreservation.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "${api.crossorigin}")
 @RestController
+@RequestMapping("${api.uri}/reservation")
 public class ReservationController {
 
   private ReservationService reservationService;
