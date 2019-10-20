1ccef36d8df338f9d0b7aac1dadd4f0b0c4bf39f
Victor <vdcommits@outlook.com>
2019-10-20 16:29:45 +0200

    Stylechange to colums and added dynamic page titles

--- account-reservation-fe/src/app/app.component.ts
+++ account-reservation-fe/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'account-reservation';
+  header_title: string;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    const pageTitle = this.header_title;
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        const child = this.activatedRoute.firstChild;
+        if(child.snapshot.data['title']){
+          return child.snapshot.data['title'];
+        }
+        return pageTitle;
+      })
+    ).subscribe((ttl: string) => {
+      this.header_title = ttl;
+    });
+  }
 }
